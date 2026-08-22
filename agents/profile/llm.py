4826a7e35404401c7a98d709026cabf8bad2368b
Thomas Serna <thomasalejandroserna@gmail.com>
2026-08-22 18:54:29 -0500

    chore: cambio de modelo de LLM

--- agents/profile/llm.py
+++ agents/profile/llm.py
@@ -6,7 +6,7 @@ from langchain_groq import ChatGroq
 load_dotenv()
 
 llm = ChatGroq(
-    model="openai/gpt-oss-120b",
+    model="openai/gpt-oss-20b",
     temperature=0,
     api_key=os.getenv("GROQ_API_KEY")
 )
